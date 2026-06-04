136b591380c9d530fe5e33f5bbeedf96190b03ad
Kharade-Shivani <shivanikharade18@gmail.com>
2026-06-04 13:55:40 +0530

    shivani c

--- src/User/Home.jsx
+++ src/User/Home.jsx
@@ -175,9 +175,9 @@ const projectLocations = [
   },
   // New India location with 100+ projects
   {
-    company: "ACCUON PROJECTS & ENGINEERS",
+   
     country: "INDIA",
-    city: "Pune, Maharashtra",
+    
     lat: 18.5204,
     lng: 73.8567,
     isSpecial: true,
@@ -1725,7 +1725,7 @@ const projectLocations = [
               <svg className="w-3 h-3" fill="currentColor" viewBox="0 0 20 20">
                 <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v3.586L7.707 9.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 10.586V7z" clipRule="evenodd" />
               </svg>
-              <span>{location.projectsCount} Projects Completed</span>
+          <span>Projects ({location.projectsCount})</span>
             </div>
           </div>
         )}
